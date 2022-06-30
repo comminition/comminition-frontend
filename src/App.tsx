4c1cbb3f98539df17bd3bf8be1501a0612c3fb4c
BHyeonKim <rlaqhguse@gmail.com>
2022-06-30 22:57:45 +0900

    markdown: GNB markdown #10

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 
 import LoginPage from 'routes/LoginPage';
+import MainPage from 'routes/MainPage';
 
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<MainPage />} />
       <Route path="login" element={<LoginPage />} />
     </Routes>
   );
---
+++ src/routes/MainPage/index.tsx
@@ -0,0 +1,9 @@
+import Gnb from 'components/Gnb';
+
+import styles from './mainPage.module.scss';
+
+const MainPage = () => {
+  return <Gnb />;
+};
+
+export default MainPage;
