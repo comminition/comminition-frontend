94fd5395aacd4fc00823138cddf281d1ec197f02
BHyeonKim <rlaqhguse@gmail.com>
2022-07-02 08:31:49 +0900

    markdown: footer #21

--- src/routes/MainPage/Footer/index.tsx
+++ src/routes/MainPage/Footer/index.tsx
@@ -1,7 +1,7 @@
 import styles from './footer.module.scss';
 
 const Footer = () => {
-  return <div className={styles.footer}>footer</div>;
+  return <div className={styles.footer}>Copyright &copy; Comminition. All Rights Reserved</div>;
 };
 
 export default Footer;
