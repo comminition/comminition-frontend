5bd80ef3a5b17bd2c305f2450c93cc9e7d6f3f8b
Low-profile <maxandy@naver.com>
2022-07-06 10:57:41 +0900

    feat: create branch

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import LoginPage from 'routes/LoginPage';
 import MainPage from 'routes/MainPage';
+import InfoPage from 'routes/InfoPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="login" element={<LoginPage />} />
+      <Route path="info" element={<InfoPage />} />
     </Routes>
   );
 };
---
+++ src/routes/InfoPage/index.tsx
@@ -0,0 +1,5 @@
+const InfoPage = () => {
+  return <div>정보게시판</div>;
+};
+
+export default InfoPage;
