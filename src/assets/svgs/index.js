5c5df77ebe4365fa62a6a83ad069b9bf6e327a8c
Low-profile <maxandy@naver.com>
2022-07-12 17:01:38 +0900

    feat: complete profile part

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -21,3 +21,4 @@ export { ReactComponent as CollectIcon } from './collect.svg';
 export { ReactComponent as InCollectIcon } from './incollect.svg';
 export { ReactComponent as PostButton } from './postbutton.svg';
 export { ReactComponent as Advertisement } from './advertisement.svg';
+export { ReactComponent as ProfileImage } from './mypageProfileImage.svg';
--- src/routes/MyPage/components/Profile/index.tsx
+++ src/routes/MyPage/components/Profile/index.tsx
@@ -1,9 +1,43 @@
 import styles from './profile.module.scss';
+import { ProfileImage } from 'assets/svgs';
 
 const Profile = () => {
   return (
-    <div>
-      <div className={styles.profile}>프로필</div>
+    <div className={styles.profile}>
+      <div className={styles.profileImage}>
+        <ProfileImage />
+      </div>
+      <div className={styles.category}>
+        <div className={styles.items}>
+          <span>닉네임</span>
+          <span>김땡</span>
+        </div>
+        <div className={styles.items}>
+          <span>분야 </span>
+          <span>Frontend Developer</span>
+        </div>
+        <div className={styles.items}>
+          <span>전공 </span>
+          <span>컴퓨터공학 전공</span>
+        </div>
+        <div className={styles.items}>
+          <span>지역</span>
+          <span>땡땡시 땡땡구</span>
+        </div>
+        <div className={styles.items}>
+          <span>이메일</span>
+          <span>binbong@naver.com</span>
+        </div>
+      </div>
+      <div className={styles.editDiv}>
+        <button type="button">프로필 수정하기</button>
+        <div className={styles.type}>
+          <div className={styles.div}>Frontend</div>
+          <div className={styles.div}>Java</div>
+          <div className={styles.div}>Html</div>
+          <div className={styles.div}>React.js</div>
+        </div>
+      </div>
     </div>
   );
 };
