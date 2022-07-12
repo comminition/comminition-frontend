91cd62b85d8a2d89a8943e56126a52895cb39f46
Low-profile <maxandy@naver.com>
2022-07-12 17:51:13 +0900

    feat: complete interest part

--- src/routes/MyPage/components/Interest/index.tsx
+++ src/routes/MyPage/components/Interest/index.tsx
@@ -3,7 +3,25 @@ import styles from './interest.module.scss';
 const Interest = () => {
   return (
     <div>
-      <div className={styles.interest}>관심직군</div>
+      <div className={styles.interest}>
+        <span>관심직군</span>
+        <div className={styles.interestBody}>
+          <div className={styles.interestBodyItems}>
+            <p>직군1</p>
+            <p>Google</p>
+          </div>
+          <hr />
+          <div className={styles.interestBodyItems}>
+            <p>직군2</p>
+            <p>NAVER</p>
+          </div>
+          <hr />
+          <div className={styles.interestBodyItems}>
+            <p>직군3</p>
+            <p>Kakao</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
