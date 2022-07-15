ec2c02ed98c7f4ef09830e63cfbf5b162816085e
BHyeonKim <rlaqhguse@gmail.com>
2022-07-15 23:51:23 +0900

    chore: removed http-proxy-middleware as cors problem solved

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: process.env.REACT_APP_BASEURL,
+  baseURL: process.env.REACT_APP_BASEURL,
   headers: {
     'Content-Type': 'application/json',
   },
