609f21a83b0968c50516b704f968288a0f01955f
BHyeonKim <rlaqhguse@gmail.com>
2022-12-05 20:52:29 +0900

    refactor: removed unnecessary functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,11 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 
 import Layout from 'components/Layouts/Layout';
 import LoginPage from 'routes/LoginPage';
-import GetUserInfoPage from 'routes/SignupPage/GetUserInfoPage';
 import MainPage from 'routes/MainPage';
 import RecruitPage from 'routes/RecruitPage';
 import InfoPage from 'routes/InfoPage';
 import QuestionPage from 'routes/QuestionPage';
 import MyPage from 'routes/MyPage';
-import SignupLayout from 'components/Layouts/SignupLayout';
-import EmailValidationPage from 'routes/SignupPage/EmailValidationPage';
-import ConnectGitHubPage from 'routes/SignupPage/ConnectGithubPage';
-import { AnimatePresence } from 'framer-motion';
 import { Toaster } from 'react-hot-toast';
 
 const App = () => {
@@ -29,15 +24,6 @@ const App = () => {
         </Route>
         <Route path="login" element={<LoginPage />} />
       </Routes>
-      <AnimatePresence exitBeforeEnter>
-        <Routes location={location} key={location.pathname}>
-          <Route path="signup" element={<SignupLayout />}>
-            <Route path="getInfo" element={<GetUserInfoPage />} />
-            <Route path="emailValidation" element={<EmailValidationPage />} />
-            <Route path="github" element={<ConnectGitHubPage />} />
-          </Route>
-        </Routes>
-      </AnimatePresence>
       <Toaster />
     </>
   );
--- src/routes/LoginPage/index.tsx
+++ src/routes/LoginPage/index.tsx
@@ -1,61 +1,19 @@
-import useInput from 'hooks/useInput';
 import classNames from 'classnames/bind';
-import { useAppDispatch, useAppSelector } from 'redux/hooks';
-import { login } from 'redux/authSlice';
-import { FormEvent, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-import TextField from '../../components/UI/TextField';
-
 import styles from './loginPage.module.scss';
 import LoginButton from 'components/UI/Buttons/LoginButton';
 
 const cx = classNames.bind(styles);
 
 const LoginPage = () => {
-  const dispatch = useAppDispatch();
-  const store = useAppSelector((state) => state.login);
-  const navigate = useNavigate();
-
-  const {
-    handleInputChange: handleEmailChange,
-    handleBlur: handleEmailBlur,
-    isTouched: isEmailTouched,
-    isValid: isEmailValid,
-    value: enteredEmail,
-  } = useInput('email');
-  const {
-    handleInputChange: handlePasswordChange,
-    handleBlur: handlePasswordBlur,
-    isTouched: isPasswordTouched,
-    isValid: isPasswordValid,
-    value: enteredPassword,
-  } = useInput('password');
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    if (isEmailValid && isPasswordValid) {
-      dispatch(login({ email: enteredEmail, password: enteredPassword }));
-    }
-  };
-
-  const handleSignupButton = () => {
-    navigate('/signup/getInfo');
-  };
-
-  useEffect(() => {
-    if (store.isAuthenticated) navigate('/');
-  }, [navigate, store.isAuthenticated]);
-
   return (
     <div className={cx('loginPage')}>
-      <form className={cx('loginForm')} onSubmit={handleSubmit}>
+      <div className={cx('loginForm')}>
         <h1>
           <mark>MJU</mark> Comminition
         </h1>
         <h2>로그인</h2>
         <LoginButton />
-      </form>
+      </div>
     </div>
   );
 };
